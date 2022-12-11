fed482344200c63946f0d3625be563e39f9eaff8
Octavian Dima <3403191+WopsS@users.noreply.github.com>
2022-12-11 21:27:04 +0100

    Exclude WIL from install

--- cmake/deps/ConfigureAndIncludeWil.cmake
+++ cmake/deps/ConfigureAndIncludeWil.cmake
@@ -1,7 +1,9 @@
 option(WIL_BUILD_PACKAGING "" OFF)
 option(WIL_BUILD_TESTS "" OFF)
 
-add_subdirectory(deps/wil)
+# EXCLUDE_FROM_ALL - Exclude this library from "install" since it doesn't have
+# an INSTALL flag.
+add_subdirectory(deps/wil EXCLUDE_FROM_ALL)
 
 mark_as_advanced(
   FAST_BUILD
