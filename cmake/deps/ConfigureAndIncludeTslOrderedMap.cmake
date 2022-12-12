168c63bc2148432b445320ddc8306dfffe54c6e6
Octavian Dima <3403191+WopsS@users.noreply.github.com>
2022-12-12 20:29:26 +0100

    Exclude TSL ordered map from install target

--- cmake/deps/ConfigureAndIncludeTslOrderedMap.cmake
+++ cmake/deps/ConfigureAndIncludeTslOrderedMap.cmake
@@ -1,2 +1,6 @@
-add_subdirectory(deps/tsl-ordered-map)
+add_subdirectory(
+  deps/tsl-ordered-map
+  EXCLUDE_FROM_ALL # Exclude this library from "install" since it doesn't have
+                   # an INSTALL flag.
+)
 target_compile_definitions(ordered_map INTERFACE NOMINMAX)
--- cmake/deps/ConfigureAndIncludeWil.cmake
+++ cmake/deps/ConfigureAndIncludeWil.cmake
@@ -1,9 +1,11 @@
 option(WIL_BUILD_PACKAGING "" OFF)
 option(WIL_BUILD_TESTS "" OFF)
 
-# EXCLUDE_FROM_ALL - Exclude this library from "install" since it doesn't have
-# an INSTALL flag.
-add_subdirectory(deps/wil EXCLUDE_FROM_ALL)
+add_subdirectory(
+  deps/wil
+  EXCLUDE_FROM_ALL # Exclude this library from "install" since it doesn't have
+                   # an INSTALL flag.
+)
 
 mark_as_advanced(
   FAST_BUILD
